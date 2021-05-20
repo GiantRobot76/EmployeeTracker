9d59e19986794ed4279e18b846beac72b1cf02ee
Jon Bowman <jon.m.bowman@gmail.com>
2021-05-20 16:37:12 -0600

    Added Initial SQL DB Design Query

---
+++ employee_trackerDB.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE employees(
+id INTEGER AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+id INTEGER AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INTEGER,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+id INTEGER AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INTEGER,
+MANAGER_ID INTEGER,
+PRIMARY KEY(id)
+
+) 
